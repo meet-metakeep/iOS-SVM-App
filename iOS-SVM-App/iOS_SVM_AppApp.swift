d98a650032bd193a7ed23ee87c1d3b74e70eb298
Meet-metakeep <meet@metakeep.xyz>
2025-08-29 00:14:20 +0530

    Initial Commit

---
+++ iOS-SVM-App/iOS_SVM_AppApp.swift
@@ -0,0 +1,32 @@
+//
+//  iOS_SVM_AppApp.swift
+//  iOS-SVM-App
+//
+//  Created by Meet  on 29/08/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct iOS_SVM_AppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
